2acf29b5ecb10106db7f37045470999b738e6868
Nicholas Handaja <nhandaja@protonmail.ch>
2017-07-28 16:40:33 -0700

    added basic test; separated index html into partials

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,8 +2,17 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get pages_index_url
+    get root_path
     assert_response :success
+  end
+
+  test "check section ids exist" do
+  	get root_path
+  	assert_response :success
+
+	assert_select "section#about"
+	assert_select "section#features"
+	assert_select "section#contact"
   end
 
 end
