dffca8e772c123aaee327cdfcd2fa47a49cba951
Nicholas Handaja <nhandaja@protonmail.ch>
2017-08-01 14:58:40 -0700

    fixed home bottom on navbar

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
   	get root_path
   	assert_template 'pages/index'
+    assert_select "a[href=?]", "#page-top"
   	assert_select "a[href=?]", "#about"
   	assert_select "a[href=?]", "#features"
   	assert_select "a[href=?]", "#contact"
@@ -13,6 +14,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   	get media_path
   	assert_template 'static_pages/media'
+    assert_select "a[href=?]", "/"
   	assert_select "a[href=?]", "/#about"
   	assert_select "a[href=?]", "/#features"
   	assert_select "a[href=?]", "/#contact"
@@ -21,6 +23,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   	get jobs_path
   	assert_template 'static_pages/jobs'
+    assert_select "a[href=?]", "/"
   	assert_select "a[href=?]", "/#about"
   	assert_select "a[href=?]", "/#features"
   	assert_select "a[href=?]", "/#contact"
