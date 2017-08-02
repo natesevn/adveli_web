dc2c1e11681294138600d37d92274bd0e74e5237
Nicholas Handaja <nhandaja@protonmail.ch>
2017-08-01 20:44:33 -0700

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/jobs', to: 'static_pages#jobs'
   resources :jobs
   root 'pages#index'
   
   get '/media', to: 'static_pages#media'
-  get '/jobs', to: 'static_pages#jobs'
+  
 end
